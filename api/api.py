657a4f163f2886b3dbfb8be920030687a2ff92a0
Simon Mayer <simon@simonmayer.co.uk>
2024-07-04 03:38:05 +0100

    move matches.py

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from api.matches import init_matches
+from api.matches.matches import init_matches
 from api.sign_in import init_auth_routes
 
 app = Flask(__name__)
