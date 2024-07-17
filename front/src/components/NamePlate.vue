c8a692f48a2a70804cb255250454918bca9e0fb5
Simon Mayer <simon@simonmayer.co.uk>
2024-07-17 06:05:27 +0100

    Dynamically style name plates

--- front/src/components/NamePlate.vue
+++ front/src/components/NamePlate.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="name-container">
     <div class="prism">
-      <div class="face front">{{ name }}</div>
+      <div :class="['face', 'front', sizeClass]">
+        <div class="text">{{ name }}</div>
+      </div>
       <div class="face rear"></div>
     </div>
   </div>
@@ -12,11 +14,47 @@ export default {
   name: 'NamePlate',
   props: {
     name: String
+  },
+  computed: {
+    sizeClass() {
+      return `size-${this.name.length}`;
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@mixin set-font-size($size, $width, $height) {
+  $characterWidth: 1.13125;
+
+  $fontSize: 8px;
+  @if ($size < ($width / ($characterWidth * 16px))) {
+    $fontSize: 16px;
+  } @else if ($size < ($width / ($characterWidth * 14px))) {
+    $fontSize: 14px;
+  } @else if (ceil($size / 2) < ($width / ($characterWidth * 14px))) {
+    $fontSize: 14px;
+  } @else if (ceil($size / 2) < ($width / ($characterWidth * 13px))) {
+    $fontSize: 13px;
+  } @else if (ceil($size / 2) < ($width / ($characterWidth * 12px))) {
+    $fontSize: 12px;
+  } @else if (ceil($size / 2) < ($width / ($characterWidth * 11px))) {
+    $fontSize: 11px;
+  } @else if (ceil($size / 2) < ($width / ($characterWidth * 10px))) {
+    $fontSize: 10px;
+  } @else if (ceil($size / 3) < ($width / ($characterWidth * 10px))) {
+    $fontSize: 10px;
+  } @else if (ceil($size / 3) < ($width / ($characterWidth * 9.5px))) {
+    $fontSize: 9.5px;
+  } @else if (ceil($size / 3) < ($width / ($characterWidth * 8.5px))) {
+    $fontSize: 8.5px;
+  }
+
+  $lineCount: floor($height / $fontSize);
+  font-size: $fontSize;
+  line-height: ($height / $lineCount);
+}
+
 .name-container {
   perspective: 1000px;
 
@@ -24,31 +62,54 @@ export default {
     position: relative;
     width: 30px;
     transform-style: preserve-3d;
-    transform: rotateX(-30deg) rotateY(30deg);
+    transform: rotateX(-30deg) rotateY(15deg);
     height: 30px;
 
     .face {
       position: absolute;
       backface-visibility: hidden;
-      padding: 10px;
       box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 
       &.front {
-        height: 16px;
         transform: translateZ(30px);
+        height: 32px;
+        width: 100px;
+        padding: 2px 4px;
         background-color: #4f200f;
-        border-top: solid 1px #666;
-        font-family: 'Georgia', serif;
-        min-width: 32px;
-        text-transform: uppercase;
-        font-weight: bold;
-        text-shadow: 0.5px 0.5px #c76c3f;
+        border-top: solid 1px #666666;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        .text {
+          color: #ffffff;
+          font-family: 'Georgia', serif;
+          font-weight: bold;
+          max-height: 100%;
+          max-width: 100%;
+          overflow: hidden;
+          word-break: break-all;
+          text-align: center;
+          text-transform: uppercase;
+          text-shadow: 0.5px 0.5px #c76c3f;
+        }
+
+        // Iterate and apply the mixin for font-size based on size class
+        @for $i from 1 through 32 {
+          &.size-#{$i} {
+            .text {
+              @include set-font-size($i, 100px, 32px);
+            }
+          }
+        }
       }
 
       &.rear {
-        transform: rotateX(-45deg) rotateY(360deg) translateZ(0.5px) translateX(4.5px);
+        transform: rotateX(-45deg) translateX(1.5px);
         height: 32px;
         width: 32px;
+        padding: 10px;
         background-color: #271810;
       }
     }
