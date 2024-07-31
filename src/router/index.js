56cc49e1832afc0324b8d791c52407048aa18b68
Simon Mayer <simon@simonmayer.co.uk>
2024-07-31 17:25:23 +0100

    Set title of app, and dynamically per page

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,47 @@ import SignIn from '@/components/SignIn.vue';
 import CreateMatch from '@/components/CreateMatch.vue';
 
 const routes = [
-    { path: '/', component: SignIn },
-    { path: '/create-match', component: CreateMatch },
-    { path: '/matches', component: MatchList },
-    { path: '/matches/:id', component: MatchDashboard, props: true }
+    {
+        path: '/',
+        component: SignIn,
+        meta: { title: 'Sign In' }
+    },
+    {
+        path: '/create-match',
+        component: CreateMatch,
+        meta: { title: 'Create Match' }
+    },
+    {
+        path: '/matches',
+        component: MatchList,
+        meta: { title: 'Your Matches' }
+    },
+    {
+        path: '/matches/:id',
+        component: MatchDashboard,
+        props: true,
+        meta: { title: 'Match #{id}' }
+    }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    const appName = 'Gin Rummy';
+    let title = to.meta.title ? appName + ' — ' + to.meta.title : appName;
+
+    for (const key in to.params) {
+        if (Object.prototype.hasOwnProperty.call(to.params, key)) {
+            const regex = new RegExp(`{${key}}`, 'g');
+            title = title.replace(regex, to.params[key]);
+        }
+    }
+
+    document.title = title;
+    next();
 });
 
 export default router;
